b399cd9353566cea8414e1414139edcae6154215
Youssef Danlos <sephir.danlos@gmail.com>
2026-07-04 23:22:57 +0200

    vault backup: 2026-07-04 23:22:57

---
+++ Scripts/remove-color-tags.js
@@ -0,0 +1,37 @@
+import type {
+  CompileContext,
+  CompileInput,
+  CompileManuscriptInput,
+  CompileSceneInput,
+} from "..";
+import { CompileStepKind, makeBuiltinStep } from "./abstract-compile-step";
+
+const COLORTAGS_REGEX = /<span style="color(.*?)>/gm;
+
+export const RemoveStrikethroughsStep = makeBuiltinStep({
+  id: "remove-strikethroughs",
+  description: {
+    name: "Remove Strikethroughs",
+    description: "Removes struck-through ~~text~~.",
+    availableKinds: [CompileStepKind.Scene, CompileStepKind.Manuscript],
+    options: [],
+  },
+  compile(input: CompileInput, context: CompileContext): CompileInput {
+    if (context.kind === CompileStepKind.Scene) {
+      return (input as CompileSceneInput[]).map((sceneInput) => {
+        return {
+          ...sceneInput,
+          contents: sceneInput.contents.replace(COLORTAGS_REGEX, () => ""),
+        };
+      });
+    } else {
+      return {
+        ...(input as CompileManuscriptInput),
+        contents: (input as CompileManuscriptInput).contents.replace(
+          COLORTAGS_REGEX,
+          () => ""
+        ),
+      };
+    }
+  },
+});
