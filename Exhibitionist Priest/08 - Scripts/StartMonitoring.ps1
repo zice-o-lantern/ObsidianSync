04d627daa63a95075f7f354fdf8d30f7310d046e
Youssef Danlos <sephir.danlos@gmail.com>
2026-07-12 22:19:06 +0200

    vault backup: 2026-07-12 22:19:06

--- Exhibitionist Priest/08 - Scripts/StartMonitoring.ps1
+++ Exhibitionist Priest/08 - Scripts/StartMonitoring.ps1
@@ -9,8 +9,8 @@
     $action = { $path = $Event.SourceEventArgs.FullPath
                 $changeType = $Event.SourceEventArgs.ChangeType
                 $logline = "$(Get-Date), $changeType, $path"
-                "D:\shin\Documents\Renpy\Exhibitionist Priest\renpy_parser\main.py" "D:\shin\Documents\ObsidianSync\Exhibitionist Priest\01 - Zettelkastem\Styrofoam Castle\EN\renpy.md" "D:\shin\Documents\Renpy\Exhibitionist Priest\game\story\styrofoam_castle.rpy"
-                "D:\shin\Documents\Renpy\Exhibitionist Priest\renpy_parser\main.py" "D:\shin\Documents\ObsidianSync\Exhibitionist Priest\01 - Zettelkastem\Styrofoam Castle\FR\renpy.md" "D:\shin\Documents\Renpy\Exhibitionist Priest\temp\styrofoam_castle_fr.rpy"
+                python "D:\shin\Documents\Renpy\Exhibitionist Priest\renpy_parser\main.py" "D:\shin\Documents\ObsidianSync\Exhibitionist Priest\01 - Zettelkastem\Styrofoam Castle\EN\renpy.md" "D:\shin\Documents\Renpy\Exhibitionist Priest\game\story\styrofoam_castle.rpy"
+                python "D:\shin\Documents\Renpy\Exhibitionist Priest\renpy_parser\main.py" "D:\shin\Documents\ObsidianSync\Exhibitionist Priest\01 - Zettelkastem\Styrofoam Castle\FR\renpy.md" "D:\shin\Documents\Renpy\Exhibitionist Priest\temp\styrofoam_castle_fr.rpy"
                 python "D:\shin\Documents\Renpy\Exhibitionist Priest\renpy_parser\localisation.py" "D:\shin\Documents\Renpy\Exhibitionist Priest\game\tl\french\story\styrofoam_castle.rpy" "D:\shin\Documents\Renpy\Exhibitionist Priest\game\story\styrofoam_castle.rpy" "D:\shin\Documents\Renpy\Exhibitionist Priest\temp\styrofoam_castle_fr.rpy"
                 Add-content "D:\log.txt" -value $logline
             }    
